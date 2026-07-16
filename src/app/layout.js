48cd70e243dba663d0951c38702954a01c943944
himanshutiwariidev <himanshu.tiwarii.dev@gmail.com>
2026-07-16 13:37:23 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import FlyingBirdLayout from "@/components/flyingbirdlayout";
 import "./globals.css";
 import ChatBot from "@/components/Chatbot";
@@ -11,6 +12,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FXH7P79ED7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FXH7P79ED7');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col">
         <Navbar />
         <FlyingBirdLayout>{children}</FlyingBirdLayout>
